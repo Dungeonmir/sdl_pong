4df9ec778ffa2637365cbb234c678ae7ddc1a9c7
dungeonmir <dungeonmir@gmail.com>
2020-12-26 14:01:12 +0300

    added platform collision, mouse input

--- sdltest/main.cpp
+++ sdltest/main.cpp
@@ -3,6 +3,7 @@
 #include <iostream>
 #include <vector>
 #include <chrono>
+#include <random>
 
 #include "RenderWindow.h"
 #include "Entity.h"
@@ -18,7 +19,9 @@ int main(int argc, char* argv[])
 	
 	int screenWidth = 1280;
 	int screenHeight = 800;
-	int ballRadius = 16;
+	
+	int xMouse = 0;
+	int yMouse = 0;
 	if (SDL_Init(SDL_INIT_VIDEO) > 0)
 	{
 		std::cout << "Hm STD_Init HAS FAILED(((  SDL_ERROR: " << SDL_GetError() << std::endl;
@@ -54,14 +57,27 @@ int main(int argc, char* argv[])
 		i.setSize(32, 64);
 	}
 	//k  
+	
 	double ballAngle = M_PI / 6;
 	float ballY = 50;
 	float ballX = 80;
-	float speed = 200.0f;
+	float speed = 0.0f;
+	float fspeed = 200.0f;
+	int maxSpeed = 1500;
 	bool gameRunning = true;
+	bool pause = true;
 	SDL_Event event;
+	const Uint8* currentKeyStates = SDL_GetKeyboardState(NULL);
 	while (gameRunning)
 	{
+		if (pause == true)
+		{
+			speed = 0;
+		}
+		else if (pause == false)
+		{
+			speed = fspeed;
+		}
 		tp2 = std::chrono::system_clock::now();
 		std::chrono::duration<float>elapsedTime = tp2 - tp1;
 		tp1 = tp2;
@@ -75,16 +91,45 @@ int main(int argc, char* argv[])
 			}
 			if (event.type ==SDL_KEYDOWN)
 			{
-				gameRunning = false;
+				if (currentKeyStates[SDL_SCANCODE_Q])
+				{
+					gameRunning = false;
+				}
+				if (currentKeyStates[SDL_SCANCODE_SPACE])
+				{
+					if (pause == true) {pause = false;}
+					else if (pause == false) {pause = true;}
+				}
+				if (currentKeyStates[SDL_SCANCODE_R])
+				{
+					std::cout << "Ball speed = " << speed<<std::endl;
+					fspeed += 200.0f;
+				}
+				
+			}
+			if (event.type == SDL_MOUSEMOTION)
+			{
+				SDL_GetMouseState(&xMouse, &yMouse);
+				
 			}
 			if (event.type == SDL_WINDOWEVENT)
 			{
+				
 					if(event.window.event == SDL_WINDOWEVENT_RESIZED)
 					{
 						
 						window.getWindowSize(&screenWidth, &screenHeight);
 						std::cout <<"Weight "<< screenWidth << std::endl;
 						std::cout <<"Height "<< screenHeight << std::endl;
+					}
+					if (event.window.event == SDL_WINDOWEVENT_MAXIMIZED or 
+						event.window.event == SDL_WINDOWEVENT_HIDDEN or 
+						event.window.event == SDL_WINDOWEVENT_MINIMIZED or 
+						event.window.event == SDL_WINDOWEVENT_MOVED or
+						event.window.event == SDL_WINDOWEVENT_RESIZED
+						)
+					{
+						pause = true;
 					}
 					
 				
@@ -106,48 +151,63 @@ int main(int argc, char* argv[])
 				
 				if (ballY < 0)
 				{
+					
 					ballAngle = M_PI - ballAngle;
 				}
-				std::cout << ballAngle << "\n";
+				
 				i.setX(ballX);
 				i.setY(ballY);
 				if (ballY > screenHeight / window.getMultiplier() - i.getH())
 				{
+
 					ballAngle =M_PI - ballAngle;
 					
 				}
-				std::cout << ballAngle << "\n";
+				
 				i.setX(ballX);
 				i.setY(ballY);
 				if (ballX <= 0)
 				{
-					ballAngle = 2*M_PI -ballAngle;
+					ballX += 1;
+					ballAngle = 2 * M_PI - ballAngle;
 				}
-				std::cout << ballAngle << "\n";
+				
 				i.setX(ballX);
 				i.setY(ballY);
 				if (ballX > screenWidth/window.getMultiplier()-i.getW())
 				{
+					ballX -= 1;
 					ballAngle =2*M_PI - ballAngle ;
 				}
-				std::cout << ballAngle << "\n";
+				
 				i.setX(ballX);
 				i.setY(ballY);
 				if (intersect(i, etop_border) == true)
 				{
-					std::cout << " top ";
+					ballY += 1;
+					
 					ballAngle =  M_PI -  ballAngle;
 				}
-				std::cout << ballAngle << "\n";
+				
 				i.setX(ballX);
 				i.setY(ballY);
 				if (intersect(i, ebottom_border) == true)
 				{
-					std::cout << " bottom ";
+					ballY -= 1;
+					
 					ballAngle = M_PI - ballAngle;
 				}
-
-				std::cout << ballAngle << "\n";
+				if (intersect(i, platforms.at(0)) == true)
+				{
+					if (fspeed < maxSpeed && pause ==false)
+					{
+						fspeed += 100;
+					}
+					
+					ballAngle = ((rand() % 75) + 25)*M_PI/180;
+					
+				}
+				
 				i.setX(ballX);
 				i.setY(ballY);
 			
@@ -155,6 +215,8 @@ int main(int argc, char* argv[])
 		}
 		for (Entity& i : platforms)
 		{
+			i.setX(xMouse/4-(i.getW()/2));
+			i.setY(yMouse/4-(i.getH()/2));
 			window.render(i);
 		}
 		
